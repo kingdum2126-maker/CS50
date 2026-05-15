052ac70b559aa84bb257ebd2f371890b5bbff1ee
kingdum2126-maker <kingdum2126@gmail.com>
2026-05-15 19:11:21 -0400

    first commit

---
+++ exercise2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{ 
+    int scoreone = 10;
+    int scoretwo = 5;
+    int sum = scoreone + scoretwo;
+    printf("The sum is %d\n", sum);// your code goes here
+    return 0;
+}
---
+++ exercise3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{printf("My name is Dan.\n");
+ printf("I am learning C,\n");
+    printf("I live in Norton, Ma.");
+    return 0;
+}
