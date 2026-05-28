c88d3b9a3d6390083a6a5190344abf284fd6e79c
Dan <kingdum2126@gmail.com>
2026-05-27 21:09:40 -0400

    completed c translations

---
+++ exercise3.py
@@ -0,0 +1,3 @@
+print("My name is Dan.")
+print("I am learning Python.")
+print("I live in Norton, Ma.")
---
+++ exercise5.py
@@ -0,0 +1,4 @@
+age = int(input("how old are you?"))
+print(age)
+print("You are", age, "years old")
+      
